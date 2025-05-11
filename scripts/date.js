8d41ed33ffd77ec5e0a345151577e8a2d62ba8db
Daberechi <daberelevi@gmail.com>
2025-05-11 03:37:57 +0100

    Udated

--- scripts/date.js
+++ scripts/date.js
@@ -1,19 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Set the current year in the footer
-    const currentYear = new Date().getFullYear();
-    const yearSpan = document.querySelector('footer p:first-child');
-    yearSpan.innerHTML = yearSpan.innerHTML.replace('2025', currentYear);
+document.addEventListener("DOMContentLoaded", () => {
+  // Set the current year in the footer
+  const currentYear = new Date().getFullYear();
+  const yearSpan = document.querySelector("footer p:first-child");
+  yearSpan.innerHTML = yearSpan.innerHTML.replace("2025", currentYear);
 
-    // Set the last modified date
-    const lastModified = document.getElementById('lastModified');
-    const options = {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: false
-    };
-    lastModified.textContent = `Last Update: ${new Date(document.lastModified).toLocaleString('en-US', options)}`;
+  // Set the last modified date
+  const lastModified = document.getElementById("lastModified");
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+  lastModified.textContent = `Last Update: ${new Date(
+    document.lastModified
+  ).toLocaleString("en-US", options)}`;
 });
